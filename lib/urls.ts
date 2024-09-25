a2a65d07e47b7555966ebd4f0456ee71de0ba69c
Wagner Santos <7467450+wagoid@users.noreply.github.com>
2024-09-25 11:18:52 -0300

    fix: nome do bucket nos URLs de audio

--- lib/urls.ts
+++ lib/urls.ts
@@ -3,4 +3,4 @@ import { Audio } from '@/lib/modules/recordings/Recording.entity'
 export const buildAudioUrl = (
   audio: Pick<Audio, 'fileId'>,
   objectStoreUrl: string,
-) => `${objectStoreUrl}/estudio/audios/${audio.fileId}.mp3`
+) => `${objectStoreUrl}/up-estudio/audios/${audio.fileId}.mp3`
